c0ff8acf9afb6ac2cbea0dcf7ec347a4597c8d16
DEKUK\uk-tchild <terry.child@asmpt.com>
2015-03-21 08:21:07 +0000

    Key repeats are based on an interval now.

--- src/escape.js
+++ src/escape.js
@@ -17,13 +17,6 @@
 				yoffset: 0
 			};
 
-			// status
-			var status = {
-				x: 1,
-				y: 1,
-				d: 2
-			};
-
 			// levels
 			var map = {
 				width: 20,
@@ -44,6 +37,17 @@
 				]
 			};
 
+			// status
+			var status = {
+				x: 1,
+				y: 1,
+				lastX: 1,
+				lastY: 1,
+				moving: false,
+				facing: 2,
+				key: -1
+			};
+
 			// load images
 			var imagesLoading = 0;
 
@@ -55,7 +59,7 @@
 				image.onload = function() {
 					imagesLoading--;
 					if(imagesLoading==0) {
-						draw();
+						draw(true);
 					}
 				}
 				return image;
@@ -72,15 +76,19 @@
 
 
 			// draw
-			function draw() {
+			function draw(everything) {
 				if(view.width && view.height && imagesLoading==0) {
-					for(y=0; y<map.height; y++) {
-						for(x=0; x<map.width; x++) {
-							context.drawImage(images[map.map[y].substr(x,1)], x*tileSize+view.xoffset, y*tileSize+view.yoffset);
+					if(everything) {
+						for(y=0; y<map.height; y++) {
+							for(x=0; x<map.width; x++) {
+								context.drawImage(images[map.map[y].substr(x,1)], x*tileSize+view.xoffset, y*tileSize+view.yoffset);
+							}
 						}
+					} else {
+						context.drawImage(images[map.map[status.lastY].substr(status.lastX,1)], status.lastX*tileSize+view.xoffset, status.lastY*tileSize+view.yoffset);
 					}
 
-					context.drawImage(images[status.d], status.x*tileSize+view.xoffset, status.y*tileSize+view.yoffset);
+					context.drawImage(images[status.facing], status.x*tileSize+view.xoffset, status.y*tileSize+view.yoffset);
 				}				
 			}
 			
@@ -94,42 +102,83 @@
 				view.xoffset = (canvas.width-(view.width*tileSize))/2;
 				view.yoffset = (canvas.height-(view.height*tileSize))/2;
 
-				draw();
+				draw(true);
 			}
 			resize();
 			$(window).on("resize", resize);
 
 
 			// simulate
-			$(window).keydown(function(e) {
+			var timer;
+
+			function move() {
+				console.log("key is "+status.key);
+				status.facing = status.key;
+
 				var newX = status.x;
 				var newY = status.y;
-
-				switch(e.which) {
-					case 38: // up
-						status.d = 0;
+				
+				switch(status.facing) {
+					case 0:
 						newY--;
 						break;
-					case 39: // right
-						status.d = 1;
+					case 1:
 						newX++;
 						break;
-					case 40: // down
-						status.d = 2;
+					case 2:
 						newY++;
 						break;
-					case 37: // left
-						status.d = 3;
+					case 3:
 						newX--;
 						break;
 				}
 
 				if(map.map[newY].substr(newX,1)===" ") {
+					status.lastX = status.x;
+					status.lastY = status.y;
 					status.x=newX;
 					status.y=newY;
-				}
 
-				draw();
+					draw(false);
+				}
+			}
+
+			$(window).on("keydown", function(e) {
+				switch(e.which) {
+					case 38: // up
+						status.key = 0;
+						break;
+					case 39: // right
+						status.key = 1;
+						break;
+					case 40: // down
+						status.key = 2;
+						break;
+					case 37: // left
+						status.key = 3;
+						break;
+				}
+				if(status.key>=0) {
+					if(!timer) {
+						move();
+						timer = setInterval(move, 250);
+					}
+				}
+			});
+
+			$(window).on("keyup", function(e) {
+				switch(e.which) {
+					case 38: // up
+					case 39: // right
+					case 40: // down
+					case 37: // left
+						status.key = -1;
+						if(timer) {
+							clearInterval(timer);
+							timer = null;
+						}
+						break;
+				}
 			});
 
 		})();
